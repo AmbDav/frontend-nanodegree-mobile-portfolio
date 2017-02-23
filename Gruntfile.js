93d6c064d9d8df8f46a8c5b01ccb6c2cab0bb14f
Amber Davenport <adavenpo@deusdatsolutions.com>
2017-02-23 12:53:05 -0500

    Starting pointing for the last 3 on speed insight

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,92 +1,98 @@
 module.exports = function(grunt){
 
-grunt.initConfig({
-    responsive_images: {
-      	dev: {
-	        options: {
-	          engine: 'im',
-	          sizes: [{
-	            width: '180px',
-	            suffix: '180',
-	            quality: 80
-	          }]
-	        },
+	grunt.initConfig({
+		responsive_images: {
+			dev: {
+				options: {
+					engine: 'im',
+					sizes: [{
+						width: '180px',
+						suffix: '180',
+						quality: 80
+					}]
+				},
 
-			files: [{
-			          expand: true,
-			          src: ['*.{gif,jpg,png}'],
-			          cwd: 'images_src/',
-			          dest: 'img/'
-			        }]
-		}
-	},
+				files: [{
+					expand: true,
+					src: ['*.{gif,jpg,png}'],
+					cwd: 'images_src/',
+					dest: 'img/'
+				}]
+			}
+		},
 	htmlmin: {                                     // Task
 	    dist: {                                      // Target
 	      options: {                                 // Target options
-	        removeComments: true,
-	        collapseWhitespace: true
+	      	removeComments: true,
+	      	collapseWhitespace: true,
+	      	minifyJS: true,
+	      	minifyCSS: true,
+	      	removeOptionalTags: true
 	      },
 	      files: {                                   // Dictionary of files
 	        'index.html': 'html_src/index.html',     // 'destination': 'source'
-	      }
-	    },
+	    }
+	},
 	    dev: {                                       // Another target
 	      options: {                                 // Target options
-	        removeComments: true,
-	        collapseWhitespace: true
+	      	removeComments: true,
+	      	collapseWhitespace: true,
+	      	minifyJS: true,
+	      	minifyCSS: true,
+	      	removeOptionalTags: true
 	      },
 	      files: {
-	        'index.html': 'html_src/index.html',
+	      	'index.html': 'html_src/index.html',
 	      }
-	    }
-  	},
+	  }
+	},
   	imagemin: {                          // Task
-	    dynamic: {                         // Another target
-	      options: {                       // Target options
-	        optimizationLevel: 6,
-	        svgoPlugins: [{ removeViewBox: false }],
-	    },
-	      files: [{
-	        expand: true,                  // Enable dynamic expansion
-	        cwd: 'image_src',                   // Src matches are relative to this path
-	        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
-	        dest: 'img/'                  // Destination path prefix
-	      }]
-	    }
+	    dist: {                         // Another target
+		    options: {                       // Target options
+		    	optimizationLevel: 9,
+		    	svgoPlugins: [{ removeViewBox: false }],
+		    },
+		    files: [{
+		        expand: true,                  // Enable dynamic expansion
+		        cwd: 'image_src',                   // Src matches are relative to this path
+		        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+		        dest: 'img/'                  // Destination path prefix
+		    }]
+		}
 	},
 
 	clean: {
-      dev: {
-        src: ["images"],
-      },
-    },
+		dev: {
+			src: ["images"],
+		},
+	},
 
 	mkdir: {
-	    dev: {
-	        options: {
-	          create: ['img']
-	        }
-	    },
+		dev: {
+			options: {
+				create: ['img']
+			}
+		},
 	},
 
 	copy: {
-	      dev: {
-	        files: [{
-	          expand: true,
-	          cwd: 'images_src',
-	          src: '*.{gif,jpg,png}',
-	          dest: 'img/'
-	        }]
-	      },
-	    },
-	  });
-  
-  grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-mkdir');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.registerTask('default', ['htmlmin', 'imagemin']);
+		dev: {
+			files: [{
+				expand: true,
+				cwd: 'images_src',
+				src: '*.{gif,jpg,png}',
+				dest: 'img/'
+			}]
+		},
+	},
+});
+
+	grunt.loadNpmTasks('grunt-responsive-images');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-mkdir');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.registerTask('default', ['htmlmin', 'imagemin']);
 };
